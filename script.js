fdd74ffdc2fbfa19036e113f8bc1f0bf6c6b2d91
Raghav-dev <rhk4133@gmail.com>
2026-02-13 18:28:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,84 +1,54 @@
-let songs = [
+let songs=[
 "h13lbNkUaEg","sf7VoyW_5ro","EtGh9oC2SZ0","yh3C2JU-m_Y",
-"1PxT9i4-uTc","Q-_cu_78eIA","0pVMxbQh-Lc","NeXbmEnpSz0",
-"palMj0iq-3g","LbrJZgyqp5w","vipdDXKHT_0","ElIizBi-rEc",
-"DL8BsPDe4ck","N5BmQz4AmFI","CQSzGF9VAak","pPGcYXZhCPY",
-"YjoKyFJf4CU","aorAeMA06i0","vmu53OX935A","wc-pzBaSiPA",
-"PMzTLWTWLZU","g5O5ufz8w34","5Eqb_-j3FDA","uXgzCjAv-9k",
-"Zu6z3qUPu1s","sX4Bxks_VlI","hoNb6HuNmU0","LK7-_dgAVQE",
-"Pm7sWFzcPes","yu8nxs1gw48","wiur_AGatGU","LAdp3ZHeP4Q",
-"rUeyfai1ddc","FCDAnPFJUPA","AN8-o7ckg6k"
+"1PxT9i4-uTc","Q-_cu_78eIA","0pVMxbQh-Lc","NeXbmEnpSz0"
 ];
 
 let player;
-let currentIndex = 0;
-let currentDisc = null;
-let likedSongs = JSON.parse(localStorage.getItem("liked")) || [];
+let currentIndex=0;
+let liked=JSON.parse(localStorage.getItem("fav"))||[];
 
-const grid = document.getElementById("songGrid");
-const progress = document.getElementById("progress");
-const volumeSlider = document.getElementById("volume");
-const miniTitle = document.getElementById("miniTitle");
+const grid=document.getElementById("songGrid");
+const progress=document.getElementById("progress");
+const volume=document.getElementById("volume");
+const miniTitle=document.getElementById("miniTitle");
 
-songs.forEach((id,index)=>{
-  const card=document.createElement("div");
-  card.className="card";
+function render(list){
+  grid.innerHTML="";
+  list.forEach((id,index)=>{
+    grid.innerHTML+=`
+    <div class="card">
+      <div class="disc" id="disc${index}"
+      style="background-image:url('https://img.youtube.com/vi/${id}/hqdefault.jpg')"></div>
+      <button onclick="playSong(${songs.indexOf(id)})">Play</button>
+    </div>`;
+  });
+}
 
-  card.innerHTML=`
-    <div class="disc" id="disc${index}"
-    style="background-image:url('https://img.youtube.com/vi/${id}/hqdefault.jpg')"></div>
-
-    <div class="song-name">Song ${index+1}</div>
-
-    <div class="bottom">
-      <span>▶</span>
-      <button class="play-btn" onclick="playSong(${index})">Play</button>
-    </div>
-  `;
-
-  grid.appendChild(card);
-});
+render(songs);
 
 function onYouTubeIframeAPIReady(){
   player=new YT.Player('youtubePlayer',{
-    height:'0',
-    width:'0',
+    height:'0',width:'0',
     videoId:songs[0],
     playerVars:{controls:0},
     events:{
-      'onReady':()=>{
-        setInterval(updateProgress,1000);
-      },
-      'onStateChange':(event)=>{
-        if(event.data===0){
-          nextSong();
-        }
+      'onStateChange':e=>{
+        if(e.data===0)nextSong();
       }
     }
   });
 }
 
-function playSong(index){
-  if(currentDisc){
-    currentDisc.classList.remove("playing");
-  }
-
-  currentIndex=index;
-  player.loadVideoById(songs[index]);
+function playSong(i){
+  currentIndex=i;
+  player.loadVideoById(songs[i]);
   player.playVideo();
-
-  currentDisc=document.getElementById("disc"+index);
-  currentDisc.classList.add("playing");
-
-  miniTitle.innerText="Song "+(index+1);
+  miniTitle.innerText="Song "+(i+1);
 }
 
 function togglePlay(){
-  if(player.getPlayerState()==1){
-    player.pauseVideo();
-  }else{
-    player.playVideo();
-  }
+  if(player.getPlayerState()==1)player.pauseVideo();
+  else player.playVideo();
 }
 
 function nextSong(){
@@ -91,45 +61,57 @@ function prevSong(){
   playSong(currentIndex);
 }
 
-function updateProgress(){
-  if(player && player.getDuration){
-    let duration=player.getDuration();
-    let current=player.getCurrentTime();
-    if(duration>0){
-      progress.value=(current/duration)*100;
-    }
-  }
-}
-
 progress.addEventListener("input",()=>{
-  let duration=player.getDuration();
-  player.seekTo((progress.value/100)*duration);
+  let d=player.getDuration();
+  player.seekTo((progress.value/100)*d);
 });
 
-volumeSlider.addEventListener("input",()=>{
-  player.setVolume(volumeSlider.value);
+setInterval(()=>{
+  if(player&&player.getDuration){
+    let d=player.getDuration();
+    let c=player.getCurrentTime();
+    if(d>0)progress.value=(c/d)*100;
+  }
+},1000);
+
+volume.addEventListener("input",()=>{
+  player.setVolume(volume.value);
 });
 
 function likeSong(){
-  if(!likedSongs.includes(currentIndex)){
-    likedSongs.push(currentIndex);
-    localStorage.setItem("liked",JSON.stringify(likedSongs));
-    alert("❤️ Saved!");
+  if(!liked.includes(currentIndex)){
+    liked.push(currentIndex);
+    localStorage.setItem("fav",JSON.stringify(liked));
+    alert("Saved to Favorites ❤️");
   }
 }
 
-/* Swipe Support */
-let startX=0;
+function showFav(){
+  let favSongs=liked.map(i=>songs[i]);
+  render(favSongs);
+}
 
-document.addEventListener("touchstart",(e)=>{
-  startX=e.touches[0].clientX;
-});
+function showAll(){
+  render(songs);
+}
 
-document.addEventListener("touchend",(e)=>{
-  let endX=e.changedTouches[0].clientX;
-  if(endX-startX>50){
-    prevSong();
-  }else if(startX-endX>50){
-    nextSong();
+/* LOGIN */
+
+function login(){
+  let name=document.getElementById("username").value;
+  if(name){
+    localStorage.setItem("user",name);
+    document.getElementById("loginBox").style.display="none";
   }
-});
+}
+
+window.onload=function(){
+  if(localStorage.getItem("user")){
+    document.getElementById("loginBox").style.display="none";
+  }
+}
+
+/* PWA */
+if('serviceWorker'in navigator){
+  navigator.serviceWorker.register('service-worker.js');
+}
