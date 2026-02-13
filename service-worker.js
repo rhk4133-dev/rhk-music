a474da6595c84713a277e9cae2ae6898b396209a
Raghav-dev <rhk4133@gmail.com>
2026-02-13 18:48:01 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,2 +1,46 @@
-self.addEventListener("install",e=>self.skipWaiting());
-self.addEventListener("fetch",e=>{});
+const CACHE_NAME = "rhk-music-v2";
+
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./script.js",
+  "./manifest.json"
+];
+
+// INSTALL
+self.addEventListener("install", (event) => {
+  console.log("Service Worker Installing...");
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+// ACTIVATE
+self.addEventListener("activate", (event) => {
+  console.log("Service Worker Activated...");
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// FETCH (Offline First)
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      return cached || fetch(event.request);
+    })
+  );
+});
