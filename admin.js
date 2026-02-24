31301c79decd6b751c3b67f63a20aef63815a1d2
Raghav-dev <rhk4133@gmail.com>
2026-02-24 16:17:48 +0530

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,22 +1,10 @@
-function saveData() {
+function saveData(){
 
-    localStorage.setItem("liveLink",
-        document.getElementById("liveLink").value);
+    const date = document.getElementById("endDate").value;
+    const link = document.getElementById("liveLink").value;
 
-    localStorage.setItem("dateTime",
-        document.getElementById("dateTime").value);
+    if(date) localStorage.setItem("endDate", date);
+    if(link) localStorage.setItem("liveLink", link);
 
-    localStorage.setItem("slots",
-        document.getElementById("slotsInput").value);
-
-    localStorage.setItem("registration",
-        document.getElementById("regInput").value);
-
-    localStorage.setItem("selected",
-        document.getElementById("selectedInput").value);
-
-    localStorage.setItem("rejected",
-        document.getElementById("rejectedInput").value);
-
-    alert("Saved Successfully ✅");
+    alert("Settings Saved Successfully");
 }
