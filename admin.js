f07f1b6b09630a319af440fd41b77d93a76bdb85
Raghav-dev <rhk4133@gmail.com>
2026-02-24 16:26:45 +0530

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,8 +1,10 @@
 function saveData(){
 
+    const status = document.getElementById("status").value;
     const date = document.getElementById("endDate").value;
     const link = document.getElementById("liveLink").value;
 
+    localStorage.setItem("status", status);
     if(date) localStorage.setItem("endDate", date);
     if(link) localStorage.setItem("liveLink", link);
 
