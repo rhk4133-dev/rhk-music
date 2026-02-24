5ac8e88c5d16010bd6dcf06d7c73a77714cfdceb
Raghav-dev <rhk4133@gmail.com>
2026-02-24 14:28:14 +0530

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,5 +1,22 @@
-function saveData(){
-    const date=document.getElementById("endDate").value;
-    localStorage.setItem("endDate",date);
-    alert("Saved Successfully!");
+function saveData() {
+
+    localStorage.setItem("liveLink",
+        document.getElementById("liveLink").value);
+
+    localStorage.setItem("dateTime",
+        document.getElementById("dateTime").value);
+
+    localStorage.setItem("slots",
+        document.getElementById("slotsInput").value);
+
+    localStorage.setItem("registration",
+        document.getElementById("regInput").value);
+
+    localStorage.setItem("selected",
+        document.getElementById("selectedInput").value);
+
+    localStorage.setItem("rejected",
+        document.getElementById("rejectedInput").value);
+
+    alert("Saved Successfully ✅");
 }
