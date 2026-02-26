4a5930d93c0e26edcd7b7e71978b9509e455722a
Raghav-dev <rhk4133@gmail.com>
2026-02-26 14:32:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
-function enterSite() {
-  alert("Welcome to RHK Music 🎵🔥");
-}
+document.addEventListener("DOMContentLoaded", function () {
+
+    const subscribeBtn = document.getElementById("subscribeBtn");
+    const subscribeSection = document.getElementById("subscribeSection");
+    const registerSection = document.getElementById("registerSection");
+    const verification = document.getElementById("verification");
+
+    subscribeBtn.addEventListener("click", function () {
+
+        verification.innerHTML = "Verifying subscription...";
+
+        setTimeout(() => {
+
+            verification.innerHTML = "Subscription Verified ✔";
+
+            // Confetti Effect
+            confetti({
+                particleCount: 100,
+                spread: 80,
+                origin: { y: 0.6 }
+            });
+
+            setTimeout(() => {
+                subscribeSection.style.display = "none";
+                registerSection.classList.remove("hidden");
+            }, 1500);
+
+        }, 2000);
+
+    });
+
+});
