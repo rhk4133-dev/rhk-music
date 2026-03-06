9fe731b3ccaddfc022287dec856cbd005e979dc2
Raghav-dev <rhk4133@gmail.com>
2026-03-06 23:17:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,39 @@
-const sheetID = "1M75opM2LF3IDIqJE-_YTZsxpPFAcypdQxJIGJ3lUNFc";
+const sheetID="1M75opM2LF3IDIqJE-_YTZsxpPFAcypdQxJIGJ3lUNFc";
 
-const url = `https://docs.google.com/spreadsheets/d/${sheetID}/gviz/tq?tqx=out:json`;
+const url=`https://docs.google.com/spreadsheets/d/${sheetID}/gviz/tq?tqx=out:json`;
 
 fetch(url)
-.then(res => res.text())
-.then(data => {
 
-const json = JSON.parse(data.substr(47).slice(0,-2));
-const rows = json.table.rows;
+.then(res=>res.text())
 
-let teams = [];
+.then(data=>{
 
-rows.forEach(r => {
+const json=JSON.parse(data.substr(47).slice(0,-2));
+
+const rows=json.table.rows;
+
+let teams=[];
+
+let totalKills=0;
+
+rows.forEach(r=>{
 
 if(!r.c[0]) return;
 
+let team=r.c[0].v;
+
+let m1=r.c[1]?r.c[1].v:0;
+
+let m2=r.c[2]?r.c[2].v:0;
+
+let total=m1+m2;
+
+totalKills+=total;
+
 teams.push({
-team:r.c[0].v,
-m1:r.c[1] ? r.c[1].v : 0,
-m2:r.c[2] ? r.c[2].v : 0,
-total:r.c[3] ? r.c[3].v : 0
+
+team,m1,m2,total
+
 });
 
 });
@@ -33,25 +47,57 @@ teams.forEach((t,i)=>{
 let rankClass="";
 
 if(i==0) rankClass="rank1";
+
 if(i==1) rankClass="rank2";
+
 if(i==2) rankClass="rank3";
 
 html+=`
+
 <tr class="${rankClass}">
+
 <td>${i+1}</td>
+
 <td>${t.team}</td>
+
 <td>${t.m1}</td>
+
 <td>${t.m2}</td>
-<td>${t.total}</td>
+
+<td class="kill">${t.total}</td>
+
 </tr>
+
 `;
 
 });
 
-document.querySelector("#leaderboard tbody").innerHTML = html;
+document.querySelector("#leaderboard tbody").innerHTML=html;
+
+document.getElementById("totalTeams").innerText=teams.length;
+
+animateKills(totalKills);
 
 });
 
+function animateKills(kills){
+
+let count=0;
+
+let interval=setInterval(()=>{
+
+count++;
+
+document.getElementById("totalKills").innerText=count;
+
+if(count>=kills) clearInterval(interval);
+
+},30);
+
+}
+
 setInterval(()=>{
+
 location.reload();
+
 },10000);
