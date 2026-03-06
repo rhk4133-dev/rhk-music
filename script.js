2e23d12b9af3b97b41c6d603244b00ff7dcb11f1
Raghav-dev <rhk4133@gmail.com>
2026-03-06 23:20:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,103 +1,32 @@
-const sheetID="1M75opM2LF3IDIqJE-_YTZsxpPFAcypdQxJIGJ3lUNFc";
+const sheetID = "1M75opM2LF3IDIqJE-_YTZsxpPFAcypdQxJIGJ3lUNFc";
 
-const url=`https://docs.google.com/spreadsheets/d/${sheetID}/gviz/tq?tqx=out:json`;
+const url = `https://opensheet.elk.sh/${sheetID}/Sheet1`;
 
 fetch(url)
+.then(res => res.json())
+.then(data => {
 
-.then(res=>res.text())
+let table = document.getElementById("table-body");
 
-.then(data=>{
+data.sort((a,b)=> b.Kills - a.Kills);
 
-const json=JSON.parse(data.substr(47).slice(0,-2));
+data.forEach((row,index)=>{
 
-const rows=json.table.rows;
+let tr = document.createElement("tr");
 
-let teams=[];
+if(index==0) tr.classList.add("rank1");
+if(index==1) tr.classList.add("rank2");
+if(index==2) tr.classList.add("rank3");
 
-let totalKills=0;
-
-rows.forEach(r=>{
-
-if(!r.c[0]) return;
-
-let team=r.c[0].v;
-
-let m1=r.c[1]?r.c[1].v:0;
-
-let m2=r.c[2]?r.c[2].v:0;
-
-let total=m1+m2;
-
-totalKills+=total;
-
-teams.push({
-
-team,m1,m2,total
-
-});
-
-});
-
-teams.sort((a,b)=>b.total-a.total);
-
-let html="";
-
-teams.forEach((t,i)=>{
-
-let rankClass="";
-
-if(i==0) rankClass="rank1";
-
-if(i==1) rankClass="rank2";
-
-if(i==2) rankClass="rank3";
-
-html+=`
-
-<tr class="${rankClass}">
-
-<td>${i+1}</td>
-
-<td>${t.team}</td>
-
-<td>${t.m1}</td>
-
-<td>${t.m2}</td>
-
-<td class="kill">${t.total}</td>
-
-</tr>
-
+tr.innerHTML = `
+<td>${index+1}</td>
+<td>${row.Team}</td>
+<td>${row.Player}</td>
+<td class="kill">${row.Kills}</td>
 `;
 
-});
-
-document.querySelector("#leaderboard tbody").innerHTML=html;
-
-document.getElementById("totalTeams").innerText=teams.length;
-
-animateKills(totalKills);
+table.appendChild(tr);
 
 });
 
-function animateKills(kills){
-
-let count=0;
-
-let interval=setInterval(()=>{
-
-count++;
-
-document.getElementById("totalKills").innerText=count;
-
-if(count>=kills) clearInterval(interval);
-
-},30);
-
-}
-
-setInterval(()=>{
-
-location.reload();
-
-},10000);
+});
