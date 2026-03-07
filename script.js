629497e32dd4a981bc59966122033fed9bff6b06
Raghav-dev <rhk4133@gmail.com>
2026-03-07 12:17:10 +0530

    Delete script.js.

--- script.js
+++
@@ -1,9 +0,0 @@
-// Optional: Add more shake with music beat (basic effect)
-const text = document.querySelector('.welcome-text');
-const audio = document.getElementById('bg-music');
-
-audio.addEventListener('play', () => {
-  setInterval(() => {
-    text.style.transform = `scale(${1 + Math.random()*0.1}) rotate(${Math.random()*4 - 2}deg)`;
-  }, 300);
-});
