5f87ba67bcd739c2fd2738c7cd2d78eb9ab5a5e5
Raghav-dev <rhk4133@gmail.com>
2026-03-07 12:07:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,110 +1,19 @@
-function showMatch(num){
+let started=false;
 
-document.getElementById("matchImg").src="match"+num+".png";
+function startShow(){
 
-}
+if(started) return;
 
+started=true;
 
-/* MATCH DATA (UPDATE AFTER EVERY MATCH) */
+document.getElementById("music").play();
 
-const matches=[
+document.getElementById("message").style.opacity="1";
 
-{
-"RHK GAMING":10,
-"GAJAPADE":6,
-"HIRIYUR":4,
-"DON 9 9 9":5,
-"CHIKKAMAGALURU":3,
-"TEAM NARACHI":2
-},
+let teams=document.querySelectorAll(".team");
 
-{
-"RHK GAMING":7,
-"GAJAPADE":8,
-"HIRIYUR":6,
-"DON 9 9 9":3,
-"CHIKKAMAGALURU":4,
-"TEAM NARACHI":5
-}
-
-];
-
-
-/* PLAYER DATA FOR MVP */
-
-const players=[
-
-{name:"Shadow",team:"RHK GAMING",kills:12},
-{name:"Blaze",team:"GAJAPADE",kills:9},
-{name:"Ghost",team:"HIRIYUR",kills:7}
-
-];
-
-
-/* MVP CALCULATION */
-
-let mvp=players[0];
-
-players.forEach(p=>{
-
-if(p.kills>mvp.kills){
-
-mvp=p;
-
-}
-
+teams.forEach(function(team){
+team.classList.add("dance");
 });
-
-document.getElementById("mvpName").innerText="Player: "+mvp.name;
-document.getElementById("mvpTeam").innerText="Team: "+mvp.team;
-document.getElementById("mvpKills").innerText="Kills: "+mvp.kills;
-
-
-/* TEAM TOTALS */
-
-let totals={};
-
-matches.forEach(match=>{
-
-for(let team in match){
-
-if(!totals[team]){
-
-totals[team]=0;
-
-}
-
-totals[team]+=match[team];
-
-}
-
-});
-
-
-let table=document.getElementById("leaderboard");
-
-for(let team in totals){
-
-let row=table.insertRow();
-
-row.insertCell(0).innerText=team;
-row.insertCell(1).innerText=totals[team];
-
-}
-
-
-/* UPDATE MESSAGE */
-
-let nextMatch=matches.length+1;
-
-if(nextMatch<=6){
-
-document.getElementById("updateText").innerText=
-"Updated leaderboard will publish after Match "+nextMatch;
-
-}else{
-
-document.getElementById("updateText").innerText=
-"Final leaderboard published";
 
 }
